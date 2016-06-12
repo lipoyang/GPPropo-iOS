84279cafa595f58b04e0bb2b9c454dc4ef80cc8a
Bizan Nishimura <lipoyang1453@gmail.com>
2016-06-12 12:13:39 +0900

    ファームウェアの修正

--- GPduinoR3/GPduinoR3.ino
+++ GPduinoR3/GPduinoR3.ino
@@ -9,14 +9,14 @@ This software is available under NYSL(Nirunari Yakunari Sukinishiro License).
 
 // ピン番号 (デジタル)
 #define MOTOR_L_IN1    2
-#define SERVO_PWM      3
+#define SERVO0_PWM     3
 #define MOTOR_L_IN2    4
 #define MOTOR_L_PWM    5
 #define MOTOR_R_PWM    6
 #define MOTOR_R_IN1    7
 #define MOTOR_R_IN2    8
-#define LED_L_PWM      9
-#define LED_R_PWM     10
+#define SERVO1_PWM     9
+#define SERVO2_PWM     10
 
 // ピン番号 (アナログ)
 #define ANALOG_0      0
@@ -27,7 +27,7 @@ This software is available under NYSL(Nirunari Yakunari Sukinishiro License).
 #define BATT_CHECK    7
 
 // サーボ
-Servo servo;
+Servo servo[3];
 
 // バッテリー電圧チェック
 MovingAverage Vbat_MovingAve;
@@ -48,43 +48,54 @@ static int cnt_runaway;
 // (3.3V / 2) / 3.3V * 1024 =  512
 #define LOW_BATTERY    512
 
-// 点滅制御
-static int g_blink_state;
-static int g_blink_cnt;
+// 四輪操舵のモード
+static int g_mode_4ws;
 
 // サーボの調整
-static int g_servo_pol; // 極性
-static int g_servo_ofs; // オフセット
-static int g_servo_amp; // 振幅
+static int g_servo_pol[3]; // 極性
+static int g_servo_ofs[3]; // オフセット
+static int g_servo_amp[3]; // 振幅
 
-// デバッグ用
-static char dbuff[256];
+// 送信バッファ
+static char txbuff[256];
 
 /**
  * バッテリー電圧チェック
  */
 void battery_check()
 {
+    static int cnt = 0;
+    
     unsigned short Vbat = analogRead(BATT_CHECK);
     unsigned short Vbat_ave = Vbat_MovingAve.pop(Vbat);
+    
+    // 1秒ごとに電圧値送信
+    cnt++;
+    if(cnt >= 1000)
+    {
+        cnt=0;
+        
+        txbuff[0]='#';
+        txbuff[1]='B';
+        Uint16ToHex(&txbuff[2], Vbat_ave, 3);
+        txbuff[5]='$';
+        txbuff[6]='\0';
+        Serial.print(txbuff);
+    }
+    
+    // 電圧が低下したら停止
     if(Vbat_ave < LOW_BATTERY){
-      // モータ停止
-      analogWrite(MOTOR_L_PWM, 0);
-      analogWrite(MOTOR_R_PWM, 0);
-      digitalWrite(MOTOR_L_IN1, LOW);
-      digitalWrite(MOTOR_L_IN2, LOW);
-      digitalWrite(MOTOR_R_IN1, LOW);
-      digitalWrite(MOTOR_R_IN2, LOW);
-      // LEDの点滅を繰り返し続ける (復帰しない)
-      while(true){
-            // Serial.println("LOW BATTERY!");
-            led_ctrl(LED_L_PWM,   0);
-            led_ctrl(LED_R_PWM,   0);
-            delay(500);
-            led_ctrl(LED_L_PWM,   255);
-            led_ctrl(LED_R_PWM,   255);
-            delay(500);
-      }
+        // モータ停止
+        analogWrite(MOTOR_L_PWM, 0);
+        analogWrite(MOTOR_R_PWM, 0);
+        digitalWrite(MOTOR_L_IN1, LOW);
+        digitalWrite(MOTOR_L_IN2, LOW);
+        digitalWrite(MOTOR_R_IN1, LOW);
+        digitalWrite(MOTOR_R_IN2, LOW);
+        // 復帰しない
+        while(true){
+            ;
+        }
     }
 }
 
@@ -112,88 +123,43 @@ void runaway_check()
  */
 void servo_init()
 {
+    int i;
     if(EEPROM.read(0) == 0xA5){
-        g_servo_pol = (int)((signed char)EEPROM.read(1));
-        g_servo_ofs = (int)((signed char)EEPROM.read(2));
-        g_servo_amp = EEPROM.read(3);
+        for(i=0;i<3;i++){
+            g_servo_pol[i] = (int)((signed char)EEPROM.read(i*3 + 1));
+            g_servo_ofs[i] = (int)((signed char)EEPROM.read(i*3 + 2));
+            g_servo_amp[i] =                    EEPROM.read(i*3 + 3);
+        }
     }else{
-        g_servo_pol = 1;
-        g_servo_ofs = 0;
-        g_servo_amp = 90;
         EEPROM.write(0, 0xA5);
-        EEPROM.write(1, g_servo_pol);
-        EEPROM.write(2, g_servo_ofs);
-        EEPROM.write(3, g_servo_amp);
+        for(i=0;i<3;i++){
+            g_servo_pol[i] = 1;
+            g_servo_ofs[i] = 0;
+            g_servo_amp[i] = 90;
+            EEPROM.write(i*3 + 1, g_servo_pol[i]);
+            EEPROM.write(i*3 + 2, g_servo_ofs[i]);
+            EEPROM.write(i*3 + 3, g_servo_amp[i]);
+        }
     }
-    servo.attach(SERVO_PWM);
-    servo_ctrl(0);
+    servo[0].attach(SERVO0_PWM);
+    servo[1].attach(SERVO1_PWM);
+    servo[2].attach(SERVO2_PWM);
+    servo_ctrl(0,0);
+    servo_ctrl(1,0);
+    servo_ctrl(2,0);
 }
 
 /*
  * サーボ制御
  */
-void servo_ctrl(int val)
+void servo_ctrl(int ch, int val)
 {
-    int deg = 90 + ((val + g_servo_ofs) *  g_servo_amp) / 127 * g_servo_pol;
+    int deg = 90 + ((val + g_servo_ofs[ch]) *  g_servo_amp[ch]) / 127 * g_servo_pol[ch];
     
-    // sprintf(dbuff, "%4d %4d / %4d %4d %4d", val, deg, g_servo_ofs, g_servo_amp, g_servo_pol);
-    // Serial.println(dbuff);
+    // sprintf(txbuff, "%4d %4d / %4d %4d %4d", val, deg, g_servo_ofs[ch], g_servo_amp[ch], g_servo_pol[ch]);
+    // Serial.println(txbuff);
     
-    servo.write(deg);
-}
-
-/*
- * LED制御
- */
-void led_ctrl(int ch, int pwm)
-{
-    int val;
-    
-    // 自動車モードの場合
-    if(drive_mode == MODE_CAR)
-    {
-        val = (pwm > 128) ? HIGH : LOW;
-        digitalWrite(ch, val);
-    }
-    // 戦車モードの場合
-    else
-    {
-        analogWrite(ch, pwm);
-    }
-}
-
-/*
- * LED点滅
- */
-void led_blink()
-{
-    int temp; 
-    int pwm;
-    
-    switch(g_blink_state){
-    case 1:
-        g_blink_cnt++;
-        if(g_blink_cnt>=1000) g_blink_cnt = 0;
-        pwm = (g_blink_cnt >= 500) ? 255 : 0;
-        led_ctrl(LED_L_PWM, pwm);
-        led_ctrl(LED_R_PWM, pwm);
-        break;
-    case 2:
-        g_blink_cnt++;
-        if(g_blink_cnt>=500) g_blink_cnt = 0;
-        pwm = (g_blink_cnt >= 250) ? 255 : 0;
-        led_ctrl(LED_L_PWM, pwm);
-        led_ctrl(LED_R_PWM, pwm);
-        break;
-    case 3:
-        g_blink_cnt++;
-        if(g_blink_cnt >= 512*4) g_blink_cnt=0;
-        temp = g_blink_cnt / 4;
-        pwm = (temp <= 255) ? temp : (511-temp);
-        led_ctrl(LED_L_PWM, pwm);
-        led_ctrl(LED_R_PWM, pwm);
-        break;
-    }
+    servo[ch].write(deg);
 }
 
 // 初期設定
@@ -204,22 +170,16 @@ void setup() {
     // PWMの初期化
     analogWrite(MOTOR_L_PWM, 0);
     analogWrite(MOTOR_R_PWM, 0);
-    analogWrite(LED_L_PWM,   0);
-    analogWrite(LED_R_PWM,   0);
     
     // GPIOの初期化
     digitalWrite(MOTOR_L_IN1, LOW);
     digitalWrite(MOTOR_L_IN2, LOW);
     digitalWrite(MOTOR_R_IN1, LOW);
     digitalWrite(MOTOR_R_IN2, LOW);
-    digitalWrite(LED_L_PWM,   LOW);
-    digitalWrite(LED_R_PWM,   LOW);
     pinMode(MOTOR_L_IN1, OUTPUT);
     pinMode(MOTOR_L_IN2, OUTPUT);
     pinMode(MOTOR_R_IN1, OUTPUT);
     pinMode(MOTOR_R_IN2, OUTPUT);
-    pinMode(LED_L_PWM,   OUTPUT);
-    pinMode(LED_R_PWM,   OUTPUT);
     
     // モード判定
     int mode_check = analogRead( MODE_CHECK );
@@ -235,7 +195,7 @@ void setup() {
     g_lr = 0;
     cnt_runaway = 0;
     Vbat_MovingAve.init();
-    g_blink_state = 0;
+    g_mode_4ws = 0;
 }
 
 // メインループ
@@ -247,8 +207,6 @@ void loop() {
     battery_check();
     // 暴走チェック
     runaway_check();
-    // LED点滅
-    led_blink();
     
     delay(1);
 }
@@ -298,8 +256,8 @@ void ctrl_tank()
     r = (int)(g_fb - g_lr/2);
     l = (int)(g_fb + g_lr/2);
     
-    // sprintf(dbuff, "%4d %4d %4d %4d", l, r, g_fb, g_lr);
-    // Serial.println(dbuff);
+    // sprintf(txbuff, "%4d %4d %4d %4d", l, r, g_fb, g_lr);
+    // Serial.println(txbuff);
     
     ctrl_motor(0, l);
     ctrl_motor(1, r);
@@ -315,6 +273,8 @@ void SerialCom_callback(char* buff)
     unsigned short val;
     int sval, sval2;
     int deg;
+    int ch;
+    int i;
     
     cnt_runaway = 0; // 暴走チェックカウンタのクリア
     
@@ -335,6 +295,7 @@ void SerialCom_callback(char* buff)
         if(drive_mode == MODE_CAR)
         {
             ctrl_motor(0, sval);
+            ctrl_motor(1, sval);
         }
         // 戦車モードの場合
         else
@@ -345,10 +306,21 @@ void SerialCom_callback(char* buff)
         break;
         
     /* Tコマンド(旋回)
-       書式: #Txx$
+       書式: #Txxn$
+       n: 4WSモード。0のとき後輪固定、1のとき同相、2のとき逆相
        xx: 0のとき中立、正のとき右旋回、負のとき左旋回
      */
     case 'T':
+        // 4WSモード
+        if(buff[3] == '1'){
+            g_mode_4ws = 1;
+        }
+        else if(buff[3] == '2'){
+            g_mode_4ws = -1;
+        }
+        else{
+            g_mode_4ws = 0;
+        } 
         // 値の解釈
         if( HexToUint16(&buff[1], &val, 2) != 0 ) break;
         sval = (int)((signed char)val);
@@ -356,7 +328,9 @@ void SerialCom_callback(char* buff)
         // 自動車モードの場合
         if(drive_mode == MODE_CAR)
         {
-            servo_ctrl(sval);
+            servo_ctrl(0, sval);
+            servo_ctrl(1, sval);
+            servo_ctrl(2, g_mode_4ws * sval);
         }
         // 戦車モードの場合
         else
@@ -402,86 +376,86 @@ void SerialCom_callback(char* buff)
         break;
         
     /* Sコマンド(サーボ制御)
-       書式: #Sxx$
+       書式: #Sxxn$
        xx: 0のとき中立、正のとき正転、負のとき反転
+       n: サーボチャンネル 0～2
      */
     case 'S':
         // 値の解釈
         if( HexToUint16(&buff[1], &val, 2) != 0 ) break;
         sval = (int)((signed char)val);
+        // チャンネル
+        ch = (buff[3] == '1') ? 1 : ((buff[3] == '2') ? 2 : 0);
         // サーボの制御
-        servo_ctrl(sval);
+        servo_ctrl(ch, sval);
         break;
-    
-    /* Lコマンド(LED制御)
-       書式1: #Lnxx$
-       n: '1'はLED1(左)、'2'はLED2(右)
-       xx: 0のとき消灯、255のとき最大輝度で点灯
-       
-       書式2: #LAxxyy$
-       xx: LED11(左)  0のとき消灯、255のとき最大輝度で点灯
-       yy: LED12(右)  0のとき消灯、255のとき最大輝度で点灯
-       
-       書式3: #LXn$
-       n: '1'は1秒周期での点滅、'2'は0.5秒周期での点滅、'3'は輝度が周期的に変化
-          '0'は点滅停止
+        
+    /* Aコマンド(サーボの調整)
+        (1)サーボ極性設定
+           書式: #APxn$
+           n: サーボチャンネル 0～2
+           xは'+'または'-'。'+'は正転、'-'は反転。初期値は正転。
+        (2)サーボオフセット調整
+           書式: #AOxxn$
+           n: サーボチャンネル 0～2
+           xx:中央位置のオフセット
+        (3)サーボ振幅調整
+           書式: #AAnxx$
+           n: サーボチャンネル 0～2
+           xx:振れ幅
+        (4)サーボ設定値保存
+           書式: #AS$
+           サーボの設定値をEEPROMに保存する
+        (5)サーボ設定値読み出し
+            書式: #AL$
+           サーボの設定値をEEPROMから読み出し、送信する
      */
-    case 'L':
-        switch(buff[1]){
-        case '1':
-            // 値の解釈
-            if( HexToUint16(&buff[2], &val, 2) != 0 ) break;
-            led_ctrl(LED_L_PWM, val);
-            break;
-        case '2':
-            // 値の解釈
-            if( HexToUint16(&buff[2], &val, 2) != 0 ) break;
-            led_ctrl(LED_R_PWM, val);
-            break;
-        case 'A':
-            // 値の解釈
-            if( HexToUint16(&buff[2], &val, 2) != 0 ) break;
-            led_ctrl(LED_L_PWM, val);
-            if( HexToUint16(&buff[4], &val, 2) != 0 ) break;
-            led_ctrl(LED_R_PWM, val);
-            break;
-        case 'X':
-            val = (unsigned short)(buff[2] - '0');
-            if(val <= 3){
-                g_blink_state = val;
-                g_blink_cnt = 0;
-                if(g_blink_state == 0)
-                {
-                    led_ctrl(LED_L_PWM, 0);
-                    led_ctrl(LED_R_PWM, 0);
-                }
-            }
-            break;
-        }
-        break;
-    
     case 'A':
         switch(buff[1]){
         case 'P':
+            ch = (buff[3] == '1') ? 1 : ((buff[3] == '2') ? 2 : 0);
             if(buff[2] == '+'){
-                g_servo_pol = 1;
-                EEPROM.write(1, g_servo_pol);
+                g_servo_pol[ch] = 1;
             }else if(buff[2] == '-'){
-                g_servo_pol = -1;
-                EEPROM.write(1, g_servo_pol);
+                g_servo_pol[ch] = -1;
             }
             break;
         case 'O':
+            ch = (buff[4] == '1') ? 1 : ((buff[4] == '2') ? 2 : 0);
             // 値の解釈
             if( HexToUint16(&buff[2], &val, 2) != 0 ) break;
-            g_servo_ofs = (int)((signed char)val);
-            EEPROM.write(2, g_servo_ofs);
+            g_servo_ofs[ch] = (int)((signed char)val);
             break;
         case 'A':
+            ch = (buff[4] == '1') ? 1 : ((buff[4] == '2') ? 2 : 0);
             // 値の解釈
             if( HexToUint16(&buff[2], &val, 2) != 0 ) break;
-            g_servo_amp = (int)((signed char)val);
-            EEPROM.write(3, g_servo_amp);
+            g_servo_amp[ch] = (int)((unsigned char)val);
+            break;
+        case 'S':
+            for(i=0;i<3;i++){
+                EEPROM.write(i*3 + 1, g_servo_pol[i]);
+                EEPROM.write(i*3 + 2, g_servo_ofs[i]);
+                EEPROM.write(i*3 + 3, g_servo_amp[i]);
+            }
+            break;
+        case 'L':
+            //delay(200); // バッファ詰まりの応急処置 (よろしくない)
+            txbuff[0]='#';
+            txbuff[1]='A';
+            txbuff[2]='L';
+            for(i=0;i<3;i++){
+                g_servo_pol[i] = (int)((signed char)EEPROM.read(i*3 + 1));
+                g_servo_ofs[i] = (int)((signed char)EEPROM.read(i*3 + 2));
+                g_servo_amp[i] =                    EEPROM.read(i*3 + 3);
+                txbuff[3+i*5] = (g_servo_pol[i]==1) ? '+' : '-';
+                Uint16ToHex(&txbuff[4+i*5], (unsigned short)((unsigned char)g_servo_ofs[i]), 2);
+                Uint16ToHex(&txbuff[6+i*5], (unsigned short)(               g_servo_amp[i]), 2);
+            }
+            txbuff[18]='$';
+            txbuff[19]='\0';
+            Serial.print(txbuff);
+            //delay(200); // バッファ詰まりの応急処置 (よろしくない)
             break;
         }
         break;
